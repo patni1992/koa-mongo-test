35ef6408020b5d8e3e2f70f23f40641d75a33799
patni1992 <patriknilsson1992@gmail.com>
2021-03-13 23:00:49 +0700

    Updates routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core'
+import Box from '@material-ui/core/Box'
 import Header from './components/Header'
 import ControllerList from './pages/Controller/ControllerList'
+import ControllerDetail from './pages/Controller/ControllerDetail'
+
+const useStyles = makeStyles(() => ({
+  spaceTop: {
+    marginTop: '100px'
+  }
+}))
 
 function App() {
+  const { spaceTop } = useStyles()
   return (
-    <div className="App">
+    <Box className="App">
       <Header />
-      <Switch>
-        <Route path="/">
-          <ControllerList />
-        </Route>
-      </Switch>
-    </div>
+      <Box className={spaceTop}>
+        <Switch>
+          <Route component={ControllerDetail} path="/controllers/:id" />
+          <Route component={ControllerList} path="/controllers" />
+          <Redirect from="/" to="/controllers" />
+        </Switch>
+      </Box>
+    </Box>
   )
 }
 
