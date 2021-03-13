5cfd7457698943db3c1e6d653de1187eacdffce6
patni1992 <patriknilsson1992@gmail.com>
2021-03-13 23:01:25 +0700

    Setup cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 const Router = require('@koa/router')
+const cors = require('@koa/cors')
 const mongo = require('./db')
 
 mongo.connectToServer()
 
 const app = new Koa()
 const router = new Router()
+app.use(cors())
 
 router.get('/locations', async (ctx) => {
   ctx.body = await mongo.getDb().collection('locations').find().toArray()
@@ -13,4 +15,4 @@ router.get('/locations', async (ctx) => {
 
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(3000)
+app.listen(1337)
