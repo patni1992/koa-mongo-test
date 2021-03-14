d5be239cd0f56fa1f0950bd177e23eb577f3ac25
patni1992 <patriknilsson1992@gmail.com>
2021-03-14 16:46:26 +0700

    Adds route for location

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const Router = require('@koa/router')
 const cors = require('@koa/cors')
+const { ObjectId } = require('mongodb')
 const mongo = require('./db')
 
 mongo.connectToServer()
@@ -11,6 +12,18 @@ app.use(cors())
 
 router.get('/locations', async (ctx) => {
   ctx.body = await mongo.getDb().collection('locations').find().toArray()
+})
+
+router.get('/locations/:id', async (ctx) => {
+  const location = await mongo.getDb().collection('locations').findOne({ 'controllers.id': ctx.params.id })
+
+  if (!location) ctx.throw(404)
+
+  const { controllerId } = location.controllers.find((controller) => controller.id === ctx.params.id)
+
+  const boxes = await mongo.getDb().collection('boxes').find({ locationId: location._id, controllerId }).toArray()
+
+  ctx.body = { ...location, boxes }
 })
 
 app.use(router.routes()).use(router.allowedMethods())
