13f334448257b11f387908b78229bfff11a41423
patni1992 <patriknilsson1992@gmail.com>
2021-03-14 16:47:01 +0700

    Adds route for box

--- server/app.js
+++ server/app.js
@@ -26,6 +26,41 @@ router.get('/locations/:id', async (ctx) => {
   ctx.body = { ...location, boxes }
 })
 
+router.get('/boxes/:id', async (ctx) => {
+  let box = await mongo
+    .getDb()
+    .collection('boxes')
+    .aggregate([
+      { $match: { _id: ObjectId(ctx.params.id) } },
+      {
+        $lookup: {
+          from: 'parcels',
+          localField: '_id',
+          foreignField: 'boxId',
+          as: 'parcel'
+        }
+      }
+    ])
+    .toArray()
+
+  if (!box) ctx.throw(404)
+
+  box = { ...box[0] }
+
+  box.parcel = box.parcel.length ? box.parcel[0] : null
+
+  if (box.parcel) {
+    const carrier = await mongo
+      .getDb()
+      .collection('carriers')
+      .findOne({ _id: ObjectId(box.parcel.carrier) }, { projection: { username: true } })
+
+    box.parcel.carrier = carrier
+  }
+
+  ctx.body = box
+})
+
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(1337)
